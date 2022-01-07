1ee3d4e36a34e57b35121ae42d61b73926aa5dd0
o-libSyntax-o <doz-services@protonmail.com>
2022-01-06 19:27:25 -0700

    Add scene framework
    Simple layout framework for adding scenes

---
+++ assets/scenes/introScreen.js
@@ -0,0 +1,23 @@
+var introScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function introScene ()
+    {
+        Phaser.Scene.call(this, { key: 'introScene' });
+    },
+
+    preload: function ()
+    {
+        
+    },
+
+    create: function ()
+    {
+        
+    }
+
+});
+
