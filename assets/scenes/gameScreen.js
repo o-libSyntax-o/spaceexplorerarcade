2b8b7c83181eba2ceedbdedd25b382cf1d8087e5
o-libSyntax-o <ethan.quisenberry@gmail.com>
2022-01-26 21:41:23 -0700

    hitbox, ship anim, restart button
    A big update with upgrade mechanics
    - Asteroid Hitboxes
    - Ship flying animation
    - Improved Performance
    - Restart Screen (soon to be game over + score)

--- assets/scenes/gameScreen.js
+++ assets/scenes/gameScreen.js
@@ -10,42 +10,53 @@ let gameScene = new Phaser.Class({
 
   preload: function () {
     this.load.json("template", "assets/data/player.json"); //Default player data template\
+    this.load.json("shapes", 'assets/data/asteroid_shapes.json');
+    this.load.atlas("ships", "assets/images/ship.png", "assets/data/ship.json");
     this.load.image("earth", "assets/images/earth.png");
-    this.load.image("ship", "assets/images/spaceship.png");
-    this.load.image("star", "assets/images/star.png")
-    this.load.image("asteroid", "assets/images/asteroid.png")
+    this.load.image("star", "assets/images/star.png");
+    this.load.image("asteroid","assets/images/asteroid.png");
   },
 
   update: function () {
+    if (cursors.left.isDown) {
+      ship.setVelocityX(-600);
+      ship.play("theship");
+    } else if (cursors.right.isDown) {
+      ship.setVelocityX(600);
+      ship.play("theship");
 
-    if (cursors.left.isDown)
-    { 
-      ship.x -= 5;
-      ship.setAngle(-15);
-    }
-    else if (cursors.right.isDown)
-    {
-      ship.x += 5;
-      ship.setAngle(15);
-    } 
-    else{
-      ship.angle = 0
+    } else {
+      ship.setVelocityX(0);
     }
 
-    earth.data.values.score += (1 * earth.data.get("multiplier")); //Update score by multiplier every update
-
+    earth.data.values.score += 1 * earth.data.get("multiplier"); //Update score by multiplier every update
   },
 
   create: function () {
-    const text = this.add.text(0, 0, '', { font: '16px Courier', fill: '#00ff00' });//Adds the planet into scene, all game data is stored in this object
-    this.physics.world.setBounds(0,0, config.width, config.height, true, true);
+    this.physics.world.setBounds(0, 0, config.width, config.height, true, true);
+
+    const text = this.add.text(0, 0, "", {
+      font: "16px Courier",
+      fill: "#00ff00",
+    }); //Adds the planet into scene, all game data is stored in this object
     earth = this.add.image(0, 0, "earth").setAlpha(0);
-    ship = this.physics.add.image(config.width / 2, 700, 'ship')
-    asteroid_collider = this.physics.add.group();
+    ship = this.physics.add.sprite(config.width / 2, 700, "ships", 0);
     ship.setCollideWorldBounds(true);
-    ship.setScale(0.15, 0.15);
+    ship.setScale(1.5, 1.5);
 
+    this.anims.create({
+      key: "theship",
+      frameRate: 60,
+      frames: this.anims.generateFrameNames("ships", {
+        prefix: "ship",
+        start: 2,
+        end: 0,
+        zeroPad: 0,
+      }),
+      repeat: 0,
+    });
 
+    asteroid_collider = this.physics.add.group();
 
     cursors = this.input.keyboard.createCursorKeys();
 
@@ -73,7 +84,7 @@ let gameScene = new Phaser.Class({
     }
 
     //Func for saving new data to localStorage periodically...
-    let saveData = async function(data) {
+    let saveData = async function (data) {
       try {
         //Gets all data from game object and saves it
         localStorage.setItem("saveGame", JSON.stringify(earth.data.getAll()));
@@ -85,67 +96,67 @@ let gameScene = new Phaser.Class({
           e.message
         );
       }
-    }
+    };
+    
     //Asteroid logic
-    var Asteroid = new Phaser.Class({
-
-      Extends: Phaser.GameObjects.Image,
-
-      initialize:
-
-      function Asteroid (scene)
-      {
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'asteroid');
+    let Asteroid = new Phaser.Class({
+      Extends: Phaser.GameObjects.Sprite,
+      initialize: function Asteroid(scene) {
+        Phaser.GameObjects.Sprite.call(
+          this,
+          scene,
+          0,
+          0,
+          "asteroid"
+        );
       },
 
-      fire: function ()
-      {
-          var ran = Phaser.Math.Between(3, 6);
-          this.speed = Phaser.Math.Between(3,5);
-          this.setPosition(Phaser.Math.Between(5, config.width - 5), -50);
-          this.setAngle(Phaser.Math.Between(0, 360));
-          this.setScale(ran, ran);
-          this.setActive(true);
-          this.setVisible(true);
+      fire: function () {
+        let ran = Phaser.Math.FloatBetween(0.05, 0.65);
+        this.speed = Phaser.Math.Between(3, 5);
+        this.setPosition(Phaser.Math.Between(5, config.width - 5), -50);
+        this.setAngle(Phaser.Math.Between(0, 360));
+        this.setScale(ran, ran);
+        this.setActive(true);
+        this.setVisible(true);
       },
 
-      update: function ()
-      {
-          this.y += this.speed;
+      update: function () {
+        this.y += this.speed;
 
-          if (this.y > 900)
-          {
-              this.setActive(false);
-              this.setVisible(false);
-          }
-      }
-
+        if (this.y > 900) {
+          this.setActive(false);
+          this.setVisible(false);
+        }
+      },
     });
 
     asteroids = this.add.group({
       classType: Asteroid,
       maxSize: 10,
-      runChildUpdate: true
+      runChildUpdate: true,
     });
-
-    
-
     //  Create the objects in advance, so they're ready and waiting in the pool
     asteroids.createMultiple({ quantity: 10, active: false });
+
     //Timer to spawn the asteroids
-    this.time.addEvent({ delay: 250, callback: function(){
-      asteroid = asteroids.get();
-      if (asteroid)
-      {
-        asteroid_collider.add(asteroid, false);
-        asteroid.fire();
-      }
-    }, callbackScope: this, loop: true });
-    
-    function hitAsteroid(ship, asteroid_collider){
+    this.time.addEvent({
+      delay: 500,
+      callback: function () {
+        asteroid = asteroids.get();
+        if (asteroid) {
+          asteroid_collider.add(asteroid, false);
+          asteroid.fire();
+        }
+      },
+      callbackScope: this,
+      loop: true,
+    });
+
+    function hitAsteroid(ship, asteroid_collider) {
       asteroid_collider.setVisible(false);
       asteroid_collider.body.enable = false;
-      if(earth.data.get("lives") > 1){
+      if (earth.data.get("lives") > 1) {
         earth.data.values.lives--;
       } else {
         earth.data.values.lives--;
@@ -154,24 +165,39 @@ let gameScene = new Phaser.Class({
     }
     this.physics.add.overlap(asteroid_collider, ship, hitAsteroid, null, this);
 
+    function getProgress(currentScore, desiredScore){
+      return String(Math.round((100 * currentScore) / desiredScore)) + "%";
+    }
+
     // -- ALL GAME DATA ELEMENTS --
     //Load player data into the game object
     earth.setData(JSON.parse(loadData()));
     // When the data changes it will save everything to localStorage
     earth.on("changedata", function (gameObject, key, value) {
-      text.setText("Score: " + earth.data.get("score") + "\nLives: " + earth.data.get("lives"));
+      let score = earth.data.get("score")
+      text.setText(
+        "Score: " +
+           score +
+        "\nLives: " +
+          earth.data.get("lives") + 
+            "\nLevel Progress: " + 
+              String(getProgress(score, 100000))
+      );
     });
     //
     //ADD GAME LOGIC BELOW
     //
     //The base game time for calculating score
-      gameTime = this.time.addEvent({ delay: 5000, callback: function(){
+    gameTime = this.time.addEvent({
+      delay: 5000,
+      callback: function () {
         //Save Data
-        console.log(saveData());
+        saveData();
         //Add a multiplier to score every 5 seconds
         earth.data.values.multiplier += 10;
-    }, callbackScope: this, loop: true });
-    
-
+      },
+      callbackScope: this,
+      loop: true,
+    });
   },
 });
